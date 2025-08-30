67e8b6030676c83f40e4a4cdad39c8e78a7325d6
AMAN RAJ <amantk2205@gmail.com>
2025-08-31 01:41:02 +0530

    Add files via upload

---
+++ forloop.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>   // Includes all standard C++ libraries
+using namespace std;      
+
+int main() {
+    for(int i = 1; i <= 10; i = i + 1) { // Loop runs from 1 to 10 (10 times)
+        cout << "\nHello";               // Prints "Hello" on a new line each time
+    }
+    return 0;   // Exit program successfully
+}
